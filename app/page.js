639f3c74e983b92cb8e5714dfa5f4352011f1d54
Pharm-Lab <pieter.zuideveld@bluewin.ch>
2026-05-09 14:56:12 +0200

    first commit - pharmlab project created

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>PharmLab 💊</h1>
+      <p>Your pharmacy study tools. Coming soon.</p>
+    </main>
+  )
+}
